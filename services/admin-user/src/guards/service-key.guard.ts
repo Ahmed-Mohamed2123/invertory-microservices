0be4d91fe8e3480aed85fc136b6f6c7c0e48da53
ahmedshabana22 <a.shabana@distinctpro.com>
2024-05-03 09:59:05 +0300

    edit guards

---
+++ services/admin-user/src/guards/service-key.guard.ts
@@ -0,0 +1,24 @@
+import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
+import { Observable } from "rxjs";
+import { ConfigService } from "@nestjs/config";
+
+@Injectable()
+export class ServiceKeyGuard implements CanActivate {
+  constructor(private configService: ConfigService) {
+  }
+
+  canActivate(
+    context: ExecutionContext
+  ): boolean | Promise<boolean> | Observable<boolean> {
+    const rpcContext = context.switchToRpc().getContext();
+    const { properties: { headers } } = rpcContext.getMessage();
+    if (!headers) return false;
+
+    const serviceKeyHeader = headers["SERVICE_KEY"];
+
+    if (!serviceKeyHeader) return false;
+    let service_key = this.configService.get("SERVICE_KEY");
+
+    return service_key === serviceKeyHeader;
+  }
+}

343403a2a109e4f924213f621b7e7d6f50f03caa
Ahmed-Mohamed2123 <ahmedshabana646@gmail.com>
2024-09-04 19:51:51 +0300

    add some enhancements to the microservices && create a gateway deplyment and service

--- services/order/src/app.module.ts
+++ services/order/src/app.module.ts
@@ -1,40 +1,42 @@
-import { Module } from "@nestjs/common";
-import { APP_GUARD, APP_INTERCEPTOR } from "@nestjs/core";
-import { ConfigModule, ConfigService } from "@nestjs/config";
-import { MongooseModule } from "@nestjs/mongoose";
-import { ErrorInterceptor } from "./interceptors/error.interceptor";
-import { ServiceKeyGuard } from "./guards/service-key.guard";
+import {Module} from "@nestjs/common";
+import {APP_GUARD, APP_INTERCEPTOR} from "@nestjs/core";
+import {ConfigModule, ConfigService} from "@nestjs/config";
+import {MongooseModule} from "@nestjs/mongoose";
+import {ErrorInterceptor} from "./interceptors/error.interceptor";
+import {ServiceKeyGuard} from "./guards/service-key.guard";
+import {OrderModule} from "./modules/order/order.module";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: `.${process.env.NODE_ENV || "local"}.env`
-    }),
-    MongooseModule.forRootAsync({
-      useFactory: (config: ConfigService) => {
-        const host = config.get("MONGO_DB_HOST");
-        const dbName = config.get("MONGO_DB_NAME");
-        const port = +config.get("MONGO_DB_PORT");
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV || "local"}.env`
+        }),
+        MongooseModule.forRootAsync({
+            useFactory: (config: ConfigService) => {
+                const host = config.get("MONGO_DB_HOST");
+                const dbName = config.get("MONGO_DB_NAME");
+                const port = +config.get("MONGO_DB_PORT");
 
-        return {
-          uri: `mongodb://${host}:${port}`,
-          dbName: dbName
-        };
-      },
-      inject: [ConfigService],
-      imports: [ConfigModule]
-    })
-  ],
-  providers: [
-    {
-      provide: APP_GUARD,
-      useClass: ServiceKeyGuard
-    },
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: ErrorInterceptor
-    }
-  ]
+                return {
+                    uri: `mongodb://${host}:${port}`,
+                    dbName: dbName
+                };
+            },
+            inject: [ConfigService],
+            imports: [ConfigModule]
+        }),
+        OrderModule
+    ],
+    providers: [
+        {
+            provide: APP_GUARD,
+            useClass: ServiceKeyGuard
+        },
+        {
+            provide: APP_INTERCEPTOR,
+            useClass: ErrorInterceptor
+        }
+    ]
 })
 export class AppModule {
 }
